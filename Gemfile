b508f4981f984bb7b765e62968d77b952ebce4ef
nhat khoa <nhatkhoa.tin@gmail.com>
2020-11-30 19:54:44 +0700

    add edit template and update action

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+ gem 'bcrypt', '~> 3.1.7'
 
 # gem 'image_processing', '~> 1.2'
 gem 'bootsnap', '>= 1.4.2', require: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_103257) do
+ActiveRecord::Schema.define(version: 2020_11_30_105228) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2020_11_30_103257) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["password_digest"], name: "index_users_on_password_digest"
   end
 
 end
