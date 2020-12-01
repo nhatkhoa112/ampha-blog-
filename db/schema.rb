4eb34ba5e47627f22bf81030123cf8bd230f9c4f
nhat khoa <nhatkhoa.tin@gmail.com>
2020-12-01 14:31:51 +0700

    compele session 6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_105228) do
+ActiveRecord::Schema.define(version: 2020_12_01_065201) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_11_30_105228) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
     t.index ["password_digest"], name: "index_users_on_password_digest"
   end
 
