0c065249ed497872dd1162f2835b701b951f6f71
nhat khoa <nhatkhoa.tin@gmail.com>
2020-12-02 18:44:59 +0700

    Complete database of category and article_categories , complete view of category table

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
     belongs_to :user
+    has_many :article_categories
+    has_many :categories, through: :article_categories, :source => :category
     validates :title, presence: true, length: { minimum: 6, maximum: 100 }
     validates :description, presence: true, length: { minimum: 10, maximum: 300 }
-    has_many :article_categories
-    has_many :categories, through: :article_categories
 end
